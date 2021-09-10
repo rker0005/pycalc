b346e7c7bedbce5ab74d846f9b0d660b7cd7bcfe
Rebecca Kerestes <rebecca.kerestes@monash.edu>
2021-09-10 11:37:23 +1000

    add multiplication lines

--- calc.py
+++ calc.py
@@ -6,7 +6,9 @@ def compute(expression):
     if operator == '+':
         return num0 + num1
     elif operator == '-':
-        return num0 - num1    
+        return num0 - num1
+    elif operator == "*"
+    return num0 * num1 
     else:
         print('unknown operator!')
         return none
